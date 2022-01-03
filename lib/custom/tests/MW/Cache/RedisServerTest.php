915f2609e1b064e23ece034bb72a220e092d838f
Aimeos <aimeos@aimeos.org>
2022-01-03 10:45:18 +0100

    Copyright update

--- lib/custom/tests/MW/Cache/RedisServerTest.php
+++ lib/custom/tests/MW/Cache/RedisServerTest.php
@@ -8,7 +8,7 @@ namespace Aimeos\MW\Cache;
  *
  * @license LGPLv3, http://www.gnu.org/licenses/lgpl.html
  * @copyright Metaways Infosystems GmbH, 2014
- * @copyright Aimeos (aimeos.org), 2015-2021
+ * @copyright Aimeos (aimeos.org), 2015-2022
  */
 class RedisServerTest extends \PHPUnit\Framework\TestCase
 {
